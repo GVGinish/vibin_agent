d87be19e7c26c02de0ba9cf89a4b3b977351cb9d
GVGinish <gin1994ish@gmail.com>
2025-02-22 11:21:38 +0530

    add link

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,8 @@
       <meta content="Themesbrand" name="author" />
       <!-- App favicon -->
       <link rel="shortcut icon" href="assets/images/favicon.ico">
+      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
       <!-- Layout config Js -->
       @yield('link')
 
