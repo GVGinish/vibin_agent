95dfe71dcdde4f1e851030306779d10ee593b088
GVGinish <gin1994ish@gmail.com>
2025-02-27 19:08:58 +0530

    qwdsqw

--- resources/views/add_customer_page.blade.php
+++ resources/views/add_customer_page.blade.php
@@ -40,94 +40,165 @@
                         <h4 class="card-title mb-0">Add New Policy</h4>
                     </div><!-- end card header -->
                     <div class="card-body">
-                    <form class="form-steps" autocomplete="off" id="addCustomer">
-    <div class="text-center pt-3 pb-4 mb-1 d-flex justify-content-center">
-        <img src="{{asset('public/assets/images/logo-dark.png')}}" class="card-logo card-logo-dark" alt="logo dark" height="17">
-        <img src="{{asset('public/assets/images/logo-light.png')}}" class="card-logo card-logo-light" alt="logo light" height="17">
-    </div>
-    <div class="step-arrow-nav mb-4">
-        <ul class="nav nav-pills custom-nav nav-justified" role="tablist">
-            <li class="nav-item" role="presentation">
-                <button class="nav-link done" id="steparrow-gen-info-tab" data-bs-toggle="pill" data-bs-target="#steparrow-gen-info" type="button" role="tab" aria-controls="steparrow-gen-info" aria-selected="true">General</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="steparrow-description-info-tab" data-bs-toggle="pill" data-bs-target="#steparrow-description-info" type="button" role="tab" aria-controls="steparrow-description-info" aria-selected="false">Bank Details</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="pills-experience-tab" data-bs-toggle="pill" data-bs-target="#pills-experience" type="button" role="tab" aria-controls="pills-experience" aria-selected="false">Finish</button>
-            </li>
-        </ul>
-    </div>
+                  
 
-    <div class="tab-content">
-        <div class="tab-pane fade show active" id="steparrow-gen-info" role="tabpanel" aria-labelledby="steparrow-gen-info-tab">
-            <div>
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="steparrow-gen-info-username-input">User Name</label>
-                            <input type="text" class="form-control" id="steparrow-gen-info-username-input" name="username" placeholder="Enter user name" required>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="steparrow-gen-info-Fathername-input">Father Name</label>
-                            <input type="text" class="form-control" id="steparrow-gen-info-Fathername-input" name="father_name" placeholder="Enter Father name" required>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="steparrow-gen-info-mother-input">Mother Name</label>
-                            <input type="text" class="form-control" id="steparrow-gen-info-mother-input" name="mother_name" placeholder="Enter mother name" required>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="steparrow-gen-info-dob-input">Date of Birth</label>
-                            <input type="date" class="form-control" id="steparrow-gen-info-dob-input" name="dob" placeholder="Enter date of birth" required max="{{date('Y-m-d')}}">
-                        </div>
-                    </div>
-                </div>
 
-                <div class="d-flex align-items-start gap-3 mt-4">
-                    <button type="button" class="btn btn-success btn-label right ms-auto nexttab" data-nexttab="steparrow-description-info-tab">
-                        <i class="ri-arrow-right-line label-icon align-middle fs-16 ms-2"></i> Go to more info
-                    </button>
-                </div>
-            </div>
-        </div>
 
-        <div class="tab-pane fade" id="steparrow-description-info" role="tabpanel" aria-labelledby="steparrow-description-info-tab">
-            <div>
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="steparrow-gen-info-bank-input">Bank Name</label>
-                            <input type="text" class="form-control" id="steparrow-gen-info-bank-input" name="bank_name" placeholder="Enter bank name" required>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="steparrow-gen-info-ifsc-input">IFSC</label>
-                            <input type="text" class="form-control" id="steparrow-gen-info-ifsc-input" name="ifsc" placeholder="Enter IFSC" required>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="d-flex align-items-start gap-3 mt-4">
-                <button type="button" class="btn btn-light btn-label previestab" data-previous="steparrow-gen-info-tab">
-                    <i class="ri-arrow-left-line label-icon align-middle fs-16 me-2"></i> Back to General
-                </button>
-                <button type="submit" class="btn btn-success btn-label right ms-auto">
+
+<form class="form-steps" autocomplete="off" id="addCustomer">
+                            <div class="text-center pt-3 pb-4 mb-1 d-flex justify-content-center">
+                                <img src="{{asset('public/assets/images/logo-dark.png')}}" class="card-logo card-logo-dark" alt="logo dark" height="17">
+                                <img src="{{asset('public/assets/images/logo-light.png')}}" class="card-logo card-logo-light" alt="logo light" height="17">
+                            </div>
+                            <div class="step-arrow-nav mb-4">
+
+                                <ul class="nav nav-pills custom-nav nav-justified" role="tablist">
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link done" id="steparrow-gen-info-tab" data-bs-toggle="pill" data-bs-target="#steparrow-gen-info" type="button" role="tab" aria-controls="steparrow-gen-info" aria-selected="true">General</button>
+                                    </li>
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link" id="steparrow-description-info-tab" data-bs-toggle="pill" data-bs-target="#steparrow-description-info" type="button" role="tab" aria-controls="steparrow-description-info" aria-selected="false">Bank Details</button>
+                                    </li>
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link" id="pills-experience-tab" data-bs-toggle="pill" data-bs-target="#pills-experience" type="button" role="tab" aria-controls="pills-experience" aria-selected="false">Finish</button>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="tab-content">
+                                <div class="tab-pane fade show active" id="steparrow-gen-info" role="tabpanel" aria-labelledby="steparrow-gen-info-tab">
+                                    <div>
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-username-input">User Name</label>
+                                                    <input type="text" class="form-control" id="steparrow-gen-info-username-input" name="username" placeholder="Enter user name" required >
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-Fathername-input">Father Name</label>
+                                                    <input type="text" class="form-control" id="steparrow-gen-info-Fathername-input" name="father_name" placeholder="Enter Fathername" required >
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                        <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-mother-input">Mother Name</label>
+                                                    <input type="text" class="form-control" id="steparrow-gen-info-mother-input" name="mother_name" placeholder="Enter mother name" required >
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-email-input">Date of Birth</label>
+                                                    <input type="date" class="form-control" id="steparrow-gen-info--input" name="dob" placeholder="Enter date of birth" required max="{{date('Y-m-d')}}">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                        <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-nominee-input">Nominee Name</label>
+                                                    <input type="text" class="form-control" id="steparrow-gen-info-nominee-input" name="nominee_name" placeholder="Enter nominee name" required >
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-n_dob-input">Nominee BOB</label>
+                                                    <input type="date" class="form-control" id="steparrow-gen-info-n_dob-input" name="nominee_dob" placeholder="Enter nominee dob" required >
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+
+                                        <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-phone-input">Phone</label>
+                                                    <input type="text" class="form-control" id="steparrow-gen-info-phone-input" name="phone" placeholder="Enter phone number" required >
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-email-input">Email</label>
+                                                    <input type="email" class="form-control" id="steparrow-gen-info-email-input" name="email" placeholder="Enter email" required >
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                        <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-address-input">Address</label>
+                                                    <textarea type="text" class="form-control" id="steparrow-gen-info-address-input" name="address" placeholder="Enter address" required ></textarea>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-occupation-input">Occupation</label>
+                                                    <input type="text" class="form-control" id="steparrow-gen-info-occupation-input" name="occupation" placeholder="Enter occupation" required >
+                                                </div>
+                                            </div>
+                                        </div>
+                                      
+                                    </div>
+                                    <div class="d-flex align-items-start gap-3 mt-4">
+                                        <button type="button" class="btn btn-success btn-label right ms-auto nexttab nexttab" data-nexttab="steparrow-description-info-tab"><i class="ri-arrow-right-line label-icon align-middle fs-16 ms-2"></i>Go to more info</button>
+                                    </div>
+                                </div>
+                                <!-- end tab pane -->
+
+                                <div class="tab-pane fade" id="steparrow-description-info" role="tabpanel" aria-labelledby="steparrow-description-info-tab">
+                                    <div>
+                                    <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-bank-input">Bank Name</label>
+                                                    <input type="text" class="form-control" id="steparrow-gen-info-bank-input" name="bank_name" placeholder="Enter bank name" required >
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-ifdc-input">IFSC</label>
+                                                    <input type="text" class="form-control" id="steparrow-gen-info-ifdc-input" name="ifsc" placeholder="Enter ifdc" required >
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="steparrow-gen-info-account-input">Account Number</label>
+                                                    <input type="text" class="form-control" id="steparrow-gen-info-account-input" name="account_num" placeholder="Enter account number" required >
+                                                </div>
+                                            </div>
+                                        
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-start gap-3 mt-4">
+                                        <button type="button" class="btn btn-light btn-label previestab" data-previous="steparrow-gen-info-tab"><i class="ri-arrow-left-line label-icon align-middle fs-16 me-2"></i> Back to General</button>
+                                        <button type="submit" class="btn btn-success btn-label right ms-auto">
                     <i class="ri-check-line label-icon align-middle fs-16 ms-2"></i> Submit
                 </button>
-            </div>
-        </div>
-    </div>
-</form>
+                                                </div>
+                                </div>
+                                <!-- end tab pane -->
+
+                                <div class="tab-pane fade" id="pills-experience" role="tabpanel">
+                                    <div class="text-center">
+
+                                        <div class="avatar-md mt-5 mb-4 mx-auto">
+                                            <div class="avatar-title bg-light text-success display-4 rounded-circle">
+                                                <i class="ri-checkbox-circle-fill"></i>
+                                            </div>
+                                        </div>
+                                        <h5>Well Done !</h5>
+                                        <p class="text-muted">You have Successfully Signed Up</p>
+                                    </div>
+                                </div>
+                                <!-- end tab pane -->
+                            </div>
+                            <!-- end tab content -->
+                        </form>
                     </div>
                     <!-- end card body -->
                 </div>
