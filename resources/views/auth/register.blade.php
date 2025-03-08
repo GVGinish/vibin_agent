80f2837b1effcc141c8ec9d34d6f2f9e7b14914f
GVGinish <gin1994ish@gmail.com>
2025-03-08 14:04:44 +0530

    asset change

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,12 +10,12 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
       <meta content="Themesbrand" name="author" />
-      <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
-      <script src="{{asset('assets/js/layout.js')}}"></script>
-      <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
-      <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
-      <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
-      <link href="{{asset('assets/css/custom.min.css')}}" rel="stylesheet" type="text/css" />
+      <link rel="shortcut icon" href="{{asset('public/assets/images/favicon.ico')}}">
+      <script src="{{asset('public/assets/js/layout.js')}}"></script>
+      <link href="{{asset('public/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+      <link href="{{asset('public/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+      <link href="{{asset('public/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
+      <link href="{{asset('public/assets/css/custom.min.css')}}" rel="stylesheet" type="text/css" />
    </head>
    <body>
       <div class="auth-page-wrapper pt-5">
@@ -31,7 +31,7 @@
                      <div class="text-center mt-sm-5 mb-4 text-white-50">
                         <div>
                            <a href="index.html" class="d-inline-block auth-logo">
-                           <img src="{{asset('assets/images/logo-light.png')}}" alt="" height="20">
+                           <img src="{{asset('public/assets/images/logo-light.png')}}" alt="" height="20">
                            </a>
                         </div>
                      </div>
@@ -128,14 +128,14 @@
       </div>
       <!-- end auth-page-wrapper -->
       <!-- JAVASCRIPT -->
-      <script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-      <script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
-      <script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
-      <script src="{{asset('assets/libs/feather-icons/feather.min.js')}}"></script>
-      <script src="{{asset('assets/js/pages/plugins/lord-icon-2.1.0.js')}}"></script>
-      <script src="{{asset('assets/js/plugins.js')}}"></script>
-      <script src="{{asset('assets/libs/particles.js')}}/particles.js')}}"></script>
-      <script src="{{asset('assets/js/pages/particles.app.js')}}"></script>
-      <script src="{{asset('assets/js/pages/password-addon.init.js')}}"></script>
+      <script src="{{asset('public/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+      <script src="{{asset('public/assets/libs/simplebar/simplebar.min.js')}}"></script>
+      <script src="{{asset('public/assets/libs/node-waves/waves.min.js')}}"></script>
+      <script src="{{asset('public/assets/libs/feather-icons/feather.min.js')}}"></script>
+      <script src="{{asset('public/assets/js/pages/plugins/lord-icon-2.1.0.js')}}"></script>
+      <script src="{{asset('public/assets/js/plugins.js')}}"></script>
+      <script src="{{asset('public/assets/libs/particles.js')}}/particles.js')}}"></script>
+      <script src="{{asset('public/assets/js/pages/particles.app.js')}}"></script>
+      <script src="{{asset('public/assets/js/pages/password-addon.init.js')}}"></script>
    </body>
 </html>
