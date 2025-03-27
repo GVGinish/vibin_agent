00a96d14c8b71baf0fd166ad4cbbccfce49e21bd
GVGinish <gin1994ish@gmail.com>
2025-03-27 11:20:33 +0530

    asset path change

--- resources/views/policy_list.blade.php
+++ resources/views/policy_list.blade.php
@@ -3,15 +3,15 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" />
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css" />
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
-<script src="{{asset('assets/js/layout.js')}}"></script>
+<script src="{{asset('public/assets/js/layout.js')}}"></script>
 <!-- Bootstrap Css -->
-<link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('public/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
-<link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('public/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
 <!-- App Css-->
-<link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('public/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
 <!-- custom Css-->
-<link href="{{asset('assets/css/custom.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('public/assets/css/custom.min.css')}}" rel="stylesheet" type="text/css" />
 @endsection
 @section('content')
 <div class="main-content">
@@ -141,14 +141,14 @@
 </div>
 @endsection
 @section('script')
-<script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
-<script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
-<script src="{{asset('assets/libs/feather-icons/feather.min.js')}}"></script>
-<script src="{{asset('assets/js/pages/plugins/lord-icon-2.1.0.js')}}"></script>
-<script src="{{asset('assets/js/plugins.js')}}"></script>
-<script src="{{asset('assets/libs/apexcharts/apexcharts.min.js')}}"></script>
-<script src="{{asset('assets/js/pages/dashboard-blog.init.js')}}"></script>
+<script src="{{asset('public/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('public/assets/libs/simplebar/simplebar.min.js')}}"></script>
+<script src="{{asset('public/assets/libs/node-waves/waves.min.js')}}"></script>
+<script src="{{asset('public/assets/libs/feather-icons/feather.min.js')}}"></script>
+<script src="{{asset('public/assets/js/pages/plugins/lord-icon-2.1.0.js')}}"></script>
+<script src="{{asset('public/assets/js/plugins.js')}}"></script>
+<script src="{{asset('public/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{asset('public/assets/js/pages/dashboard-blog.init.js')}}"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <!--datatable js-->
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
@@ -160,8 +160,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="{{asset('assets/js/pages/datatables.init.js')}}"></script>
-<script src="{{asset('assets/js/app.js')}}"></script>
+<script src="{{asset('public/assets/js/pages/datatables.init.js')}}"></script>
+<script src="{{asset('public/assets/js/app.js')}}"></script>
 <script>
          $(document).ready(function() {
             $('#buttons-datatables-1').DataTable();
